7091fc2b98df6a76872b107ee14459fd6112ce45
Milan Kumar <milankryadav@gmail.com>
2024-02-05 15:56:58 +0000

    Added Nav and Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import BookingForm from './components/BookingForm';
+import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <Nav/>
+      <Header/>
+      <BookingForm/>
+    </>
   );
 }
 
